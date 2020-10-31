67561a0b717522392bd04d9ca5f828fc24f1abd0
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-10-31 14:34:21 +0900

    Add construction on char node

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
-import { Parser } from 'rerejs';
+import * as rerejs from 'rerejs';
 
-console.log('hello');
+interface NFA {
+  states: State[];
+  initialStates: State[];
+  acceptingStates: State[];
+}
+
+interface State {
+  transitions: Transition[];
+}
+
+interface Transition {
+  char: rerejs.Char;
+}
+
+function construct(pattern: rerejs.Pattern): NFA {
+  return construct_(pattern.child);
+}
+
+function construct_(node: rerejs.Node): NFA {
+  switch (node.type) {
+    case 'Char':
+      const d0: Transition = { char: node };
+      const q0: State = { transitions: [d0] };
+      const q1: State = { transitions: [] };
+      return {
+        states: [q0, q1],
+        initialStates: [q0],
+        acceptingStates: [q1],
+      };
+  }
+}
+
+function main() {
+  const pat = new rerejs.Parser('a').parse();
+  const res = construct(pat);
+  console.log(res);
+}
+
+if (require.main === module) {
+  main();
+}
