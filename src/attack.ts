5c92b183739cad5f5b5dcf152f3bcafc26217ca1
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-11-27 03:08:58 +0900

    指数的攻撃文字列

--- src/attack.ts
+++ src/attack.ts
@@ -1,11 +1,83 @@
-import { NonEpsilonNFA, State, Char } from './types';
+import { getLeftState, getRightState } from './directProduct';
+import {
+  NonEpsilonNFA,
+  StronglyConnectedComponentGraph,
+  DirectProductGraph,
+  State,
+  Char,
+} from './types';
 
-export function buildExponentialAttack(): void {
-  // unimplemented
+export function buildExponentialAttack(
+  nfa: NonEpsilonNFA,
+  dp: DirectProductGraph,
+  sccs: StronglyConnectedComponentGraph[],
+): string | null {
+  for (const scc of sccs) {
+    for (const source of scc.stateList) {
+      const sourceLeft = getLeftState(source);
+      const souceRight = getRightState(source);
+
+      if (sourceLeft !== souceRight) {
+        continue;
+      }
+
+      // 多重自己ループを検出
+      for (const char of scc.alphabet) {
+        let visited = false;
+        for (const destination of scc.transitions.get(source, char)) {
+          if (destination !== source) {
+            continue;
+          }
+          if (visited) {
+            let attack = '';
+            attack += pathString(nfa, nfa.initialState, new Set([sourceLeft]));
+            if (char !== null) {
+              attack += char.repeat(20);
+            } else {
+              // TODO: その他の文字の場合
+            }
+            attack += pathString(nfa, sourceLeft, nfa.acceptingStateSet);
+            return attack;
+          }
+          visited = true;
+        }
+      }
+
+      // (n, n) → (m, k) → (n, n) (m ≠ k) を検出
+      for (const viaPair of scc.stateList) {
+        const viaLeft = getLeftState(viaPair);
+        const viaRight = getRightState(viaPair);
+        if (viaLeft !== viaRight) {
+          let attack = '';
+          attack += pathString(nfa, nfa.initialState, new Set([sourceLeft]));
+          {
+            let loop = '';
+            loop += pathString(nfa, sourceLeft, new Set([viaLeft]));
+            loop += pathString(nfa, viaLeft, new Set([sourceLeft]));
+            attack += loop.repeat(20);
+          }
+          attack += pathString(nfa, sourceLeft, nfa.acceptingStateSet);
+          return attack;
+        }
+      }
+    }
+  }
+
+  // EDA 検出なし
+  return null;
 }
 
 export function buildPolynomialAttack(): void {
   // unimplemented
+}
+
+function pathString(
+  nfa: NonEpsilonNFA,
+  source: State,
+  destinations: Set<State>,
+): string {
+  // TODO: その他の文字の場合
+  return restorePath(nfa, source, destinations).join('');
 }
 
 /**
@@ -14,7 +86,7 @@ export function buildPolynomialAttack(): void {
 function restorePath(
   nfa: NonEpsilonNFA,
   source: State,
-  destination: State,
+  destinations: Set<State>,
 ): Char[] {
   /** その状態にどの状態からどの遷移でたどり着けるか */
   const referrer = new Map<State, [source: State, char: Char]>();
@@ -22,9 +94,12 @@ function restorePath(
   const queue: State[] = [];
   queue.push(source);
 
+  let foundDestination: State | null = null;
+
   while (queue.length !== 0) {
     const q = queue.shift()!;
-    if (q === destination) {
+    if (destinations.has(q)) {
+      foundDestination = q;
       break;
     }
     for (const char of nfa.alphabet) {
@@ -39,11 +114,13 @@ function restorePath(
   }
 
   const path: Char[] = [];
-  for (let q = destination; q !== source; ) {
-    const [prevState, char] = referrer.get(q)!;
-    path.push(char);
-    q = prevState;
+  if (foundDestination !== null) {
+    for (let q = foundDestination; q !== source; ) {
+      const [prevState, char] = referrer.get(q)!;
+      path.push(char);
+      q = prevState;
+    }
+    path.reverse();
   }
-  path.reverse();
   return path;
 }
