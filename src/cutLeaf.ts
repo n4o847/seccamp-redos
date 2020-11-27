0f22032560af321775d1a5eaf67f47441f954532
yuziroppe <syani4732@gmail.com>
2020-11-27 13:08:12 +0900

    LeafCut内のstateの定義を変更

--- src/cutLeaf.ts
+++ src/cutLeaf.ts
@@ -16,31 +16,44 @@ class LeafCutNFABuilder {
     // 初期状態作成
     const newInitialStateSet: Set<State> = new Set();
     const newAcceptingStateSet: Set<State> = new Set();
-    // {q0, {q0, q1}} => [q0, Set{q0,q1}]
-    const table = new Map<State, [State, Set<State>]>();
+    // '(q0, {q0, q1})' => [q0, {q0,q1}]
+    // DFAのSetを得るにはtableを引いた後に, さらにthis.dfa.tableを引けばいい
+    const table = new Map<State, [State, State]>();
 
-    for (const [q0, qs0] of this.dfa.table) {
-      const newStateTuple: [State, Set<State>] = [this.nfa.initialState, qs0];
-      const newSate = State.fromPair([this.nfa.initialState, q0]);
-      newInitialStateSet.add(newSate);
-      table.set(newSate, newStateTuple);
+    // 全状態作成
+    for (const Q0 of this.dfa.stateList) {
+      for (const q0 of this.nfa.stateList) {
+        const newStateTuple: [State, State] = [q0, Q0];
+        const newSate = State.fromPair([q0, Q0]);
+        this.newStateList.push(newSate);
+        table.set(newSate, newStateTuple);
+      }
+    }
+
+    for (const [q0, _] of this.dfa.table) {
+      const newState = State.fromPair([this.nfa.initialState, q0]);
+      newInitialStateSet.add(newState);
     }
     console.log('Initial: ', newInitialStateSet);
 
-    // FIXME: 受理状態作成(Q_fがもとのNFAのstate情報を持っている必要がある)
     for (const ass of this.dfa.acceptingStateSet) {
       const oneAcceptStateSet = this.dfa.table.get(ass)!;
       for (const q of this.nfa.stateList) {
         if (oneAcceptStateSet.has(q)) {
-          const newStateTuple: [State, Set<State>] = [q, oneAcceptStateSet];
-          const newSate = State.fromPair([q, ass]);
-          newAcceptingStateSet.add(newSate);
-          table.set(newSate, newStateTuple);
+          const newState = State.fromPair([q, ass]);
+          newAcceptingStateSet.add(newState);
         }
       }
     }
+
     console.log('Accept:', newAcceptingStateSet);
     console.log('Table:', table);
+
+    // 遷移を作成
+    for (const ns of this.newStateList) {
+      // nsから引ける辺, つまり
+      const stateTuple = table.get(ns)!;
+    }
 
     return {
       type: 'LeafCutNFA',
