c7f3001b816ff9e55e34f3b6d9c5ea1fd3007e8f
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-11-27 22:38:33 +0900

    eda.ts と attack.ts を統合

--- src/attack.ts
+++ src/attack.ts
@@ -2,13 +2,19 @@ import { getLeftState, getRightState } from './directProduct';
 import { State } from './state';
 import { NonEpsilonNFA, Char, StronglyConnectedComponentGraph } from './types';
 
-export function buildExponentialAttack(
-  nfa: NonEpsilonNFA,
-  sccs: StronglyConnectedComponentGraph[],
-): string | null {
-  const nullChar = getCharForNull(nfa.alphabet);
+export class Attacker {
+  private nfa: NonEpsilonNFA;
+  private nullChar: string;
 
-  for (const scc of sccs) {
+  constructor(nfa: NonEpsilonNFA) {
+    this.nfa = nfa;
+    this.nullChar = this.getCharForNull();
+  }
+
+  /**
+   * EDA 構造を見つけ、それに対する攻撃文字列を生成する。
+   */
+  findExponentialAttack(scc: StronglyConnectedComponentGraph): string | null {
     for (const source of scc.stateList) {
       const sourceLeft = getLeftState(source);
       const souceRight = getRightState(source);
@@ -19,27 +25,19 @@ export function buildExponentialAttack(
 
       // 多重自己ループを検出
       for (const char of scc.alphabet) {
-        let visited = false;
-        for (const destination of scc.transitions.get(source, char)) {
-          if (destination !== source) {
-            continue;
+        const selfLoops = scc.transitions
+          .get(source, char)
+          .filter((q) => q === source);
+
+        if (selfLoops.length >= 2) {
+          let attack = '';
+          attack += this.pathString(this.nfa.initialState, sourceLeft);
+          {
+            const loop = char ?? this.nullChar;
+            attack += loop.repeat(20);
           }
-          if (visited) {
-            let attack = '';
-            attack += pathString(nfa, nfa.initialState, sourceLeft, nullChar);
-            {
-              const loop = char ?? nullChar;
-              attack += loop.repeat(20);
-            }
-            for (const char of [...nfa.alphabet, null]) {
-              if (nfa.transitions.get(sourceLeft, char).length === 0) {
-                attack += char ?? nullChar;
-                break;
-              }
-            }
-            return attack;
-          }
-          visited = true;
+          attack += this.getSuffix(sourceLeft);
+          return attack;
         }
       }
 
@@ -49,54 +47,88 @@ export function buildExponentialAttack(
         const viaRight = getRightState(viaPair);
         if (viaLeft !== viaRight && viaLeft !== sourceLeft) {
           let attack = '';
-          attack += pathString(nfa, nfa.initialState, sourceLeft, nullChar);
+          attack += this.pathString(this.nfa.initialState, sourceLeft);
           {
             let loop = '';
-            loop += pathString(nfa, sourceLeft, viaLeft, nullChar);
-            loop += pathString(nfa, viaLeft, sourceLeft, nullChar);
+            loop += this.pathString(sourceLeft, viaLeft);
+            loop += this.pathString(viaLeft, sourceLeft);
             attack += loop.repeat(20);
           }
-          for (const char of [...nfa.alphabet, null]) {
-            if (nfa.transitions.get(sourceLeft, char).length === 0) {
-              attack += char ?? nullChar;
-              break;
-            }
-          }
+          attack += this.getSuffix(sourceLeft);
           return attack;
         }
       }
     }
+
+    // EDA 検出なし
+    return null;
   }
 
-  // EDA 検出なし
-  return null;
-}
-
-export function buildPolynomialAttack(): void {
-  // unimplemented
-}
-
-/**
- * null に対応する文字を探す。
- */
-function getCharForNull(alphabet: Set<Char>): string {
-  for (let c = 0x00; ; c++) {
-    const s = String.fromCharCode(c);
-    if (!alphabet.has(s)) {
-      return s;
+  /**
+   * IDA 構造を見つけ、それに対する攻撃文字列を生成する。
+   */
+  findPolynomialAttack(scc: StronglyConnectedComponentGraph): string | null {
+    // (lq, lq, rq) と (lq, rq, rq) が存在するか
+    for (const [lq, cq, rq] of scc.stateList.map((s) => s.split('_'))) {
+      if (lq === cq && scc.stateList.includes(`${lq}_${rq}_${rq}` as State)) {
+        let attack = '';
+        attack += this.pathString(this.nfa.initialState, lq as State);
+        {
+          const loop = this.pathString(lq as State, rq as State);
+          attack += loop.repeat(20);
+        }
+        attack += this.getSuffix(rq as State);
+        return attack;
+      }
     }
+
+    // IDA 検出なし
+    return null;
+  }
+
+  /**
+   * null に対応する文字を探す。
+   */
+  private getCharForNull(): string {
+    for (let c = 0x00; ; c++) {
+      const s = String.fromCharCode(c);
+      if (!this.nfa.alphabet.has(s)) {
+        return s;
+      }
+    }
+  }
+
+  /**
+   * ある状態からある状態まで遷移する文字列を探す。
+   */
+  private pathString(source: State, destination: State): string {
+    return findPath(this.nfa, source, destination)
+      .map((char) => char || this.nullChar)
+      .join('');
+  }
+
+  /**
+   * (TODO)
+   * EDA/IDA 構造からバックトラックの起こるような状態へ遷移する⽂字列を探す。
+   */
+  private getSuffix(source: State): string {
+    for (const char of [...this.nfa.alphabet, null]) {
+      if (this.nfa.transitions.get(source, char).length === 0) {
+        return char ?? this.nullChar;
+      }
+    }
+    return '';
   }
 }
 
 /**
  * NFA 上である状態からある状態までの経路 (Char の配列) を幅優先探索で復元する。
  */
-function pathString(
+function findPath(
   nfa: NonEpsilonNFA,
   source: State,
   destination: State,
-  nullChar: string,
-): string {
+): Char[] {
   /** その状態にどの状態からどの遷移でたどり着けるか */
   const referrer = new Map<State, [source: State, char: Char]>();
 
@@ -119,14 +151,14 @@ function pathString(
     }
   }
 
-  const path: string[] = [];
+  const path: Char[] = [];
 
   for (let q = destination; q !== source; ) {
     const [prevState, char] = referrer.get(q)!;
-    path.push(char ?? nullChar);
+    path.push(char);
     q = prevState;
   }
   path.reverse();
 
-  return path.join('');
+  return path;
 }
--- src/eda.ts
+++ src/eda.ts
@@ -1,5 +1,4 @@
-import { buildExponentialAttack } from './attack';
-import { getLeftState, getRightState } from './directProduct';
+import { Attacker } from './attack';
 import { buildStronglyConnectedComponents } from './scc';
 import { NonEpsilonNFA, DirectProductGraph, Message } from './types';
 
@@ -10,40 +9,17 @@ export function showMessageEDA(
   nfa: NonEpsilonNFA,
   dps: DirectProductGraph[],
 ): Message {
-  // 別の経路で同じ文字で移動して自身に戻れたらEDA
-  if (dps.some((dp) => isEDA(nfa, dp))) {
-    return { status: 'Vulnerable', message: 'Detected EDA.', attack: '' };
-  } else {
-    return { status: 'Safe', message: "Don't have EDA." };
-  }
-}
+  const attacker = new Attacker(nfa);
 
-function isEDA(nfa: NonEpsilonNFA, dp: DirectProductGraph): boolean {
-  const sccs = buildStronglyConnectedComponents(dp);
+  for (const dp of dps) {
+    const sccs = buildStronglyConnectedComponents(dp);
 
-  const attack = buildExponentialAttack(nfa, sccs);
-  if (attack !== null) {
-    console.log({ attack });
-  }
-
-  return sccs.some((scc) => {
-    // 遷移元と遷移先が同じ遷移が複数存在するかを検出
-    for (const source of scc.stateList) {
-      for (const char of scc.alphabet) {
-        const destinationArray = scc.transitions
-          .get(source, char)
-          .filter((d) => d === source);
-
-        if (destinationArray.length >= 2) {
-          return true;
-        }
+    for (const scc of sccs) {
+      const attack = attacker.findExponentialAttack(scc);
+      if (attack !== null) {
+        return { status: 'Vulnerable', message: 'Detected EDA.', attack };
       }
     }
-
-    const lrSame = scc.stateList.filter(
-      (state) => getLeftState(state) === getRightState(state),
-    );
-    // (n, n), (m, k) (m !== k)が存在(すべて同じじゃないが全て異なるわけではない)
-    return lrSame.length < scc.stateList.length && lrSame.length > 0;
-  });
+  }
+  return { status: 'Safe', message: "Don't have EDA." };
 }
